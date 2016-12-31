b9b2648ac8b3eadaff5d9c971f995cac5429092f
Lich2013 <longzy2013@gmail.com>
2016-12-31 22:19:48 +0800

    output version

--- controllers/MainController.go
+++ controllers/MainController.go
@@ -1,11 +1,16 @@
 package controllers
 
-import "fmt"
+import "github.com/astaxie/beego"
 
 type MainController struct {
 	BaseController
 }
 
-func (this *MainController) Get()  {
-	fmt.Println("main")
+func (this *MainController) Get() {
+	this.Data["json"] = map[string]string{
+		"name":   beego.BConfig.AppName,
+		"version":beego.AppConfig.String("version"),
+		"runmode":beego.BConfig.RunMode,
+	}
+	this.ServeJSON()
 }
